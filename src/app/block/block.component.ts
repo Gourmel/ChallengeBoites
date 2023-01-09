c2c6939b314e3a576bf59fd8eaac6fdd3f22c65e
Gourmel <[eric.gourmel@sncf.fr](mailto:eric.gourmel@sncf.fr)>
2023-01-09 16:28:27 +0100

    finish

---
+++ src/app/block/block.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  templateUrl: './block.component.html',
+  styleUrls: ['./block.component.css']
+})
+export class BlockComponent {
+  title = "ChallengeBoites";
+  
+  ngOnInit (){
+    this.title = 'ABC'
+  }
+}
